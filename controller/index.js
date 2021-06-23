3212c326f8d7acac28aaf76da455294ae28ad8ce
jecshcier <alsk123111@icloud.com>
2021-06-23 18:21:19 +0800

    update

--- controller/index.js
+++ controller/index.js
@@ -1,15 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Router = require("koa-router");
-const router = new Router();
-router.prefix('/snapshot/index');
-router.get('/', async (ctx, next) => {
-    await ctx.render('index', {
-        title: 'Hello Koa 2!'
-    });
-});
-function default_1(app) {
-    app.use(router.routes(), router.allowedMethods());
-}
-exports.default = default_1;
-//# sourceMappingURL=index.js.map
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const Router=require("koa-router"),router=new Router;function default_1(e){e.use(router.routes(),router.allowedMethods())}router.prefix("/snapshot/index"),router.get("/",async(e,t)=>{await e.render("index",{title:"Hello Koa 2!"})}),exports.default=default_1;
