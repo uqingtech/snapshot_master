31d6e21c09fc799d96e8bce10014e282a8048055
jecshcier <alsk123111@icloud.com>
2021-09-01 16:36:42 +0800

    update

--- process_task.js
+++ process_task.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const task_1=require("./task");task_1.createStaticDir(),task_1.clearStaticResource();
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const task_1=require("./task");(0,task_1.createStaticDir)(),(0,task_1.clearStaticResource)();
